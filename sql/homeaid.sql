66342a2e2d2f62e78f09104e4f5930f879d77cdc
pageunavailable <steevenmendenilla@gmail.com>
2021-10-11 11:58:24 +0800

    sql

--- sql/homeaid.sql
+++ sql/homeaid.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 06, 2021 at 06:50 AM
+-- Generation Time: Oct 11, 2021 at 05:57 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.10
 
@@ -29,21 +29,41 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `categories` (
   `category_code` varchar(10) NOT NULL,
-  `description` varchar(255) NOT NULL
+  `category_name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `image` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `categories`
 --
 
-INSERT INTO `categories` (`category_code`, `description`) VALUES
-('BBYSIT', 'Babysitting'),
-('CRPT', 'Carpentry'),
-('ELEC', 'Electrical'),
-('HOUSEKP', 'Housekeeping'),
-('PLMB', 'Plumbing'),
-('TECH', 'Technology'),
-('WDWRK', 'Woodworking');
+INSERT INTO `categories` (`category_code`, `category_name`, `description`, `image`) VALUES
+('CRPT', 'Carpentry', 'Wood Works, Construction, House Repairs', '../images/category_images/carpentry.jpg'),
+('ELEC', 'Electrical', 'Wiring Repairs, Electrical Installations', '../images/category_images/electrical.jpg'),
+('HOUSEKP', 'Housekeeping', 'Cooking, Cleaning, Babysitting', '../images/category_images/housekeeping.jpg'),
+('PLMB', 'Plumbing', 'Pipe Repair, Septic Tank Maintenance', '../images/category_images/plumbing.jpg'),
+('TECH', 'Technology', 'PC Repair, PC Cleaning, PC Assistance', '../images/category_images/technology.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prof_skl`
+--
+
+CREATE TABLE `prof_skl` (
+  `id` int(8) NOT NULL,
+  `category_code` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `prof_skl`
+--
+
+INSERT INTO `prof_skl` (`id`, `category_code`) VALUES
+(1, 'PLMB'),
+(3, 'HOUSEKP'),
+(5, 'CRPT');
 
 -- --------------------------------------------------------
 
@@ -57,15 +77,17 @@ CREATE TABLE `users` (
   `username` varchar(20) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `address` varchar(255) NOT NULL
+  `profile` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `username`, `email`, `password`, `address`) VALUES
-(1, 'Steeven Jan B. Mendenilla', 'pageunavailable', 'steevenjan@yahoo.com', 'ff7bd97b1a7789ddd2775122fd6817f3173672da9f802ceec57f284325bf589f', 'Rosewood Pointe, Taguig City');
+INSERT INTO `users` (`id`, `name`, `username`, `email`, `password`, `profile`) VALUES
+(1, 'Steeven Jan B. Mendenilla', 'pageunavailable', 'steevenjan@yahoo.com', 'ff7bd97b1a7789ddd2775122fd6817f3173672da9f802ceec57f284325bf589f', 'koro.jpg'),
+(3, 'Roland Renzo Tan', 'ragedwolf420', 'qrrptan@tip.edu.ph', 'ff7bd97b1a7789ddd2775122fd6817f3173672da9f802ceec57f284325bf589f', 'john.jpg'),
+(5, 'Kyle River Bacongan', 'GRUFFYX', 'pisceskyle@gmail.com', 'ff7bd97b1a7789ddd2775122fd6817f3173672da9f802ceec57f284325bf589f', 'ayako.jpg');
 
 --
 -- Indexes for dumped tables
@@ -78,10 +100,19 @@ ALTER TABLE `categories`
   ADD PRIMARY KEY (`category_code`);
 
 --
+-- Indexes for table `prof_skl`
+--
+ALTER TABLE `prof_skl`
+  ADD KEY `fk_cd` (`category_code`),
+  ADD KEY `fk_id` (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -91,7 +122,18 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `prof_skl`
+--
+ALTER TABLE `prof_skl`
+  ADD CONSTRAINT `fk_cd` FOREIGN KEY (`category_code`) REFERENCES `categories` (`category_code`),
+  ADD CONSTRAINT `fk_id` FOREIGN KEY (`id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
